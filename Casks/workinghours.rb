d2ab39aaf5b007e53ad8cd016b61eb2da3df0950
govind kumar <govind.kumar@revinate.com>
2026-04-04 11:30:14 +0530

    Update SHA checksum to latest v1.4.0 payload

--- Casks/workinghours.rb
+++ Casks/workinghours.rb
@@ -1,6 +1,6 @@
 cask "workinghours" do
   version "1.4.0"
-  sha256 "REPLACE_ME_TEMPORARILY"
+  sha256 "89cb8f04bdb5ec30da1d07f4a1e32848122bca627e11c86bd40c8e28131143ed"
   url "https://github.com/govind0229/Workplace-monitor/releases/download/v1.4.0/WorkingHours.pkg"
   name "Working Hours Monitor"
   desc "Location-based automatic time tracking and monitoring"
